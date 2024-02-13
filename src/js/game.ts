c5c8320ae191440484f50609394f5775b26e60b6
seewilds <seewilds@protonmail.com>
2024-02-12 21:49:17 -0500

    defender scales to size of the board

--- src/js/game.ts
+++ src/js/game.ts
@@ -4,20 +4,37 @@ class Defender {
   pixels: Pixel[];
   key: boolean;
   deltaX: number;
-  constructor(context: CanvasRenderingContext2D) {
+  pixelsPerPixel : number;
+  constructor(pixelsPerPixel : number, boardDimension : number, context: CanvasRenderingContext2D) {
     this.context = context;
     this.deltaX = 0;
     this.pixels = new Array(14);
-    for (let i = 0; i < 5; i++) {
-      this.pixels[i] = new Pixel(5, 5, 300 + i * 5, 440, "#20FF20");
+    this.pixelsPerPixel = pixelsPerPixel;
+    let bottom = boardDimension - this.pixelsPerPixel * 2;
+    let left = Math.round(boardDimension / 2 - 13 / 2);
+    let row1, row2, row3, row4, row5, row6, row7, row8 = 0;
+    for (row1 = 0; row1 < 13; row1++) {
+      this.pixels[row1] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + row1 * this.pixelsPerPixel, bottom, "#20FF20");
     }
-    for (let i = 5; i < 10; i++) {
-      this.pixels[i] = new Pixel(5, 5, 300 + (i - 5) * 5, 435, "#20FF20");
+    for (row2 = row1; row2 < 26; row2++) {
+      this.pixels[row2] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row2 - row1) * this.pixelsPerPixel, bottom - this.pixelsPerPixel, "#20FF20");
     }
-    for (let i = 10; i < 13; i++) {
-      this.pixels[i] = new Pixel(5, 5, 305 + (i - 10) * 5, 430, "#20FF20");
+    for (row3 = row2; row3 < 39; row3++) {
+      this.pixels[row3] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row3 - row2) * this.pixelsPerPixel, bottom - this.pixelsPerPixel * 2, "#20FF20");
     }
-    this.pixels[13] = new Pixel(5, 5, 310, 425, "#20FF20");
+    for (row4 = row3; row4 < 52; row4++) {
+      this.pixels[row4] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row4 - row3) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 3, "#20FF20");
+    }
+    for (row5 = row4; row5 < 63; row5++) {
+      this.pixels[row5] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + this.pixelsPerPixel + (row5 - row4) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 4, "#20FF20");
+    }
+    for (row6 = row5; row6 < 66; row6++) {
+      this.pixels[row6] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + 5 * this.pixelsPerPixel + (row6 - row5) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 5, "#20FF20");
+    }
+    for (row7 = row6; row7 < 66; row7++) {
+      this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 5 * this.pixelsPerPixel + 1) + (row7 - row6) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 6, "#20FF20");
+    }
+    this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 6 * this.pixelsPerPixel), bottom - this.pixelsPerPixel * 6, "#20FF20");
     window.addEventListener('keydown', (event) => this.HandleKeyDown(event));
     window.addEventListener('keyup', (event) => this.HandleKeyUp(event));
   }
@@ -28,10 +45,10 @@ class Defender {
   }
   HandleKeyDown(event: KeyboardEvent) {
     if (event.key == 'a') {
-      this.deltaX = -1;
+      this.deltaX = -10;
     }
     if (event.key == 'd') {
-      this.deltaX = 1;
+      this.deltaX = 10;
     }
   }
   HandleKeyUp(event: KeyboardEvent) {
@@ -44,6 +61,7 @@ class Defender {
   }
 }
 
+// space invaders is 128 x 128 pixels
 class Battlefield {
   canvas: HTMLCanvasElement;
   context: CanvasRenderingContext2D | null;
