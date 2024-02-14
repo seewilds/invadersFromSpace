f449d3ee3105a58c21461f72f745d29461b95714
seewilds <seewilds@protonmail.com>
2024-02-13 21:12:13 -0500

    add invaders

--- src/js/game.ts
+++ src/js/game.ts
@@ -61,6 +61,143 @@ class Defender {
   }
 }
 
+
+class Octopus {
+  context: CanvasRenderingContext2D;
+  health: number;
+  pixels: Pixel[];
+  key: boolean;
+  deltaX: number;
+  pixelsPerPixel : number;
+  constructor(pixelsPerPixel : number, boardDimension : number, context: CanvasRenderingContext2D) {
+    this.context = context;
+    this.deltaX = 0;
+    this.pixels = new Array(14);
+    this.pixelsPerPixel = pixelsPerPixel;
+    let bottom = boardDimension - this.pixelsPerPixel * 2;
+    let left = Math.round(boardDimension / 2 - 13 / 2);
+    let row1, row2, row3, row4, row5, row6, row7, row8 = 0;
+    for (row1 = 0; row1 < 4; row1++) {
+      this.pixels[row1] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + row1 * this.pixelsPerPixel, bottom, "#20FF20");
+    }
+    for (row2 = row1; row2 < 26; row2++) {
+      this.pixels[row2] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row2 - row1) * this.pixelsPerPixel, bottom - this.pixelsPerPixel, "#20FF20");
+    }
+    for (row3 = row2; row3 < 39; row3++) {
+      this.pixels[row3] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row3 - row2) * this.pixelsPerPixel, bottom - this.pixelsPerPixel * 2, "#20FF20");
+    }
+    for (row4 = row3; row4 < 52; row4++) {
+      this.pixels[row4] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row4 - row3) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 3, "#20FF20");
+    }
+    for (row5 = row4; row5 < 63; row5++) {
+      this.pixels[row5] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + this.pixelsPerPixel + (row5 - row4) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 4, "#20FF20");
+    }
+    for (row6 = row5; row6 < 66; row6++) {
+      this.pixels[row6] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + 5 * this.pixelsPerPixel + (row6 - row5) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 5, "#20FF20");
+    }
+    for (row7 = row6; row7 < 66; row7++) {
+      this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 5 * this.pixelsPerPixel + 1) + (row7 - row6) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 6, "#20FF20");
+    }
+    this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 6 * this.pixelsPerPixel), bottom - this.pixelsPerPixel * 6, "#20FF20");
+  }
+  Update() {
+    this.pixels.forEach(pixel => {
+      pixel.Update(this.context, pixel.x += this.deltaX, pixel.y);
+    });
+  }
+}
+
+class Crab {
+  context: CanvasRenderingContext2D;
+  health: number;
+  pixels: Pixel[];
+  key: boolean;
+  deltaX: number;
+  pixelsPerPixel : number;
+  constructor(pixelsPerPixel : number, boardDimension : number, context: CanvasRenderingContext2D) {
+    this.context = context;
+    this.deltaX = 0;
+    this.pixels = new Array(14);
+    this.pixelsPerPixel = pixelsPerPixel;
+    let bottom = boardDimension - this.pixelsPerPixel * 2;
+    let left = Math.round(boardDimension / 2 - 13 / 2);
+    let row1, row2, row3, row4, row5, row6, row7, row8 = 0;
+    for (row1 = 0; row1 < 4; row1++) {
+      this.pixels[row1] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + row1 * this.pixelsPerPixel, bottom, "#20FF20");
+    }
+    for (row2 = row1; row2 < 26; row2++) {
+      this.pixels[row2] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row2 - row1) * this.pixelsPerPixel, bottom - this.pixelsPerPixel, "#20FF20");
+    }
+    for (row3 = row2; row3 < 39; row3++) {
+      this.pixels[row3] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row3 - row2) * this.pixelsPerPixel, bottom - this.pixelsPerPixel * 2, "#20FF20");
+    }
+    for (row4 = row3; row4 < 52; row4++) {
+      this.pixels[row4] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row4 - row3) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 3, "#20FF20");
+    }
+    for (row5 = row4; row5 < 63; row5++) {
+      this.pixels[row5] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + this.pixelsPerPixel + (row5 - row4) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 4, "#20FF20");
+    }
+    for (row6 = row5; row6 < 66; row6++) {
+      this.pixels[row6] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + 5 * this.pixelsPerPixel + (row6 - row5) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 5, "#20FF20");
+    }
+    for (row7 = row6; row7 < 66; row7++) {
+      this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 5 * this.pixelsPerPixel + 1) + (row7 - row6) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 6, "#20FF20");
+    }
+    this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 6 * this.pixelsPerPixel), bottom - this.pixelsPerPixel * 6, "#20FF20");
+  }
+  Update() {
+    this.pixels.forEach(pixel => {
+      pixel.Update(this.context, pixel.x += this.deltaX, pixel.y);
+    });
+  }
+}
+
+class Squid {
+  context: CanvasRenderingContext2D;
+  health: number;
+  pixels: Pixel[];
+  key: boolean;
+  deltaX: number;
+  pixelsPerPixel : number;
+  constructor(pixelsPerPixel : number, boardDimension : number, context: CanvasRenderingContext2D) {
+    this.context = context;
+    this.deltaX = 0;
+    this.pixels = new Array(14);
+    this.pixelsPerPixel = pixelsPerPixel;
+    let bottom = boardDimension - this.pixelsPerPixel * 2;
+    let left = Math.round(boardDimension / 2 - 13 / 2);
+    let row1, row2, row3, row4, row5, row6, row7, row8 = 0;
+    for (row1 = 0; row1 < 4; row1++) {
+      this.pixels[row1] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + row1 * this.pixelsPerPixel, bottom, "#20FF20");
+    }
+    for (row2 = row1; row2 < 26; row2++) {
+      this.pixels[row2] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row2 - row1) * this.pixelsPerPixel, bottom - this.pixelsPerPixel, "#20FF20");
+    }
+    for (row3 = row2; row3 < 39; row3++) {
+      this.pixels[row3] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row3 - row2) * this.pixelsPerPixel, bottom - this.pixelsPerPixel * 2, "#20FF20");
+    }
+    for (row4 = row3; row4 < 52; row4++) {
+      this.pixels[row4] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + (row4 - row3) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 3, "#20FF20");
+    }
+    for (row5 = row4; row5 < 63; row5++) {
+      this.pixels[row5] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + this.pixelsPerPixel + (row5 - row4) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 4, "#20FF20");
+    }
+    for (row6 = row5; row6 < 66; row6++) {
+      this.pixels[row6] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, left + 5 * this.pixelsPerPixel + (row6 - row5) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 5, "#20FF20");
+    }
+    for (row7 = row6; row7 < 66; row7++) {
+      this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 5 * this.pixelsPerPixel + 1) + (row7 - row6) * this.pixelsPerPixel,  bottom - this.pixelsPerPixel * 6, "#20FF20");
+    }
+    this.pixels[row7] = new Pixel(this.pixelsPerPixel, this.pixelsPerPixel, (left + 6 * this.pixelsPerPixel), bottom - this.pixelsPerPixel * 6, "#20FF20");
+  }
+  Update() {
+    this.pixels.forEach(pixel => {
+      pixel.Update(this.context, pixel.x += this.deltaX, pixel.y);
+    });
+  }
+}
+
+
 // space invaders is 128 x 128 pixels
 class Battlefield {
   canvas: HTMLCanvasElement;
