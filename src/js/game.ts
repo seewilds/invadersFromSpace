500f50a33c215a4598ba3f5b199c3e7e3c77d6fe
seewilds <seewilds@protonmail.com>
2024-02-18 21:33:04 -0500

    create and implement type for shield

--- src/js/game.ts
+++ src/js/game.ts
@@ -1,4 +1,4 @@
-import { IInvader } from "./types.ts"
+import { Sprite } from "./types.ts"
 
 class Defender {
   context: CanvasRenderingContext2D;
@@ -75,18 +75,20 @@ function spriteFactory(height: number, width: number, pixelsPerPixel: number, xS
 class Shield {
   context: CanvasRenderingContext2D;
   pixels: Pixel[];
+  sprite : Sprite;
   key: boolean;
   deltaX: number;
   pixelsPerPixel: number;
   x :number;
   y :number;
-  constructor(pixelsPerPixel: number, x: number, y: number, context: CanvasRenderingContext2D) {
+  constructor(sprite : Sprite, pixelsPerPixel: number, x: number, y: number, context: CanvasRenderingContext2D) {
     this.context = context;
+    this.sprite = sprite;
     this.deltaX = 0;
     this.pixelsPerPixel = pixelsPerPixel;
     this.x = x;
     this.y = y;
-    this.pixels = spriteFactory(16, 22, pixelsPerPixel, this.x, this.y, [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268, 269, 270, 286, 287, 288, 289, 290, 291, 308, 309, 310, 311, 312, 330, 331, 332, 333, 334, 279, 280, 281, 282, 283, 284, 285, 302, 303, 304, 305, 306, 307, 325, 326, 327, 328, 329, 347, 348, 349, 350, 351], "#1FFE1F");
+    this.pixels = spriteFactory(this.sprite.rows, this.sprite.cols, this.pixelsPerPixel, this.x, this.y, this.sprite.activePixels, "#1FFE1F");
     requestAnimationFrame(this.Update.bind(this));
   }
   Update() {
@@ -121,7 +123,7 @@ class Invader {
   context: CanvasRenderingContext2D;
   health: number;
   pixels: Pixel[];
-  IInvader : IInvader;
+  sprite : Sprite;
   colour : string;
   pixelAActive: boolean;
   key: boolean;
@@ -131,9 +133,9 @@ class Invader {
   y: number;
   updateInterval: number;
   lastUpdate: number;
-  constructor(IInvader : IInvader, colour: string, pixelsPerPixel: number, x: number, y: number, context: CanvasRenderingContext2D) {
+  constructor(sprite : Sprite, colour: string, pixelsPerPixel: number, x: number, y: number, context: CanvasRenderingContext2D) {
     this.context = context;
-    this.IInvader = IInvader;
+    this.sprite = sprite;
     this.colour = colour;
     this.deltaX = 0;
     this.pixelsPerPixel = pixelsPerPixel;
@@ -141,7 +143,7 @@ class Invader {
     this.y = y;
     this.updateInterval = 200;
     this.lastUpdate = performance.now();
-    this.pixels = spriteFactory(this.IInvader.rows, this.IInvader.cols, this.pixelsPerPixel, this.x, this.y, this.IInvader.activePixels, this.colour);
+    this.pixels = spriteFactory(this.sprite.rows, this.sprite.cols, this.pixelsPerPixel, this.x, this.y, this.sprite.activePixels, this.colour);
     this.pixelAActive = true;
     requestAnimationFrame(this.Update.bind(this));
   }
@@ -149,12 +151,12 @@ class Invader {
     const deltaTime = timestamp - this.lastUpdate;
     if (deltaTime >= this.updateInterval) {
       this.context.fillStyle = "black";
-      this.context.fillRect(this.pixels[0].x - this.IInvader.activePixels[0] * this.pixelsPerPixel, this.pixels[0].y, this.IInvader.cols * this.pixelsPerPixel, this.IInvader.rows * this.pixelsPerPixel);
+      this.context.fillRect(this.pixels[0].x - this.sprite.activePixels[0] * this.pixelsPerPixel, this.pixels[0].y, this.sprite.cols * this.pixelsPerPixel, this.sprite.rows * this.pixelsPerPixel);
       if (this.pixelAActive) {
-        this.pixels = spriteFactory(this.IInvader.rows, this.IInvader.cols, this.pixelsPerPixel, this.x, this.y, this.IInvader.activePixelsAlt, this.colour)
+        this.pixels = spriteFactory(this.sprite.rows, this.sprite.cols, this.pixelsPerPixel, this.x, this.y, this.sprite.activePixelsAlt, this.colour)
         this.pixelAActive = false;
       } else {
-        this.pixels = spriteFactory(this.IInvader.rows, this.IInvader.cols, this.pixelsPerPixel, this.x, this.y, this.IInvader.activePixels, this.colour);
+        this.pixels = spriteFactory(this.sprite.rows, this.sprite.cols, this.pixelsPerPixel, this.x, this.y, this.sprite.activePixels, this.colour);
         this.pixelAActive = true;
       }
       this.pixels.forEach(pixel => {
